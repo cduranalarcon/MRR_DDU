085d1ba15294b353e0dc7f0ff2f35a41ac4ddae2
cduranalarcon <44102364+cduranalarcon@users.noreply.github.com>
2019-12-09 15:03:09 +0100

    Create __init__.py

---
+++ Lib/IMProToo-master/IMProToo/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+IMProToo
+Improved MRR Processing Tool
+"""
+from __future__ import division
+
+
+from .core import *
+from .core import __version__
+from .tools import *
