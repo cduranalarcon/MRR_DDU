afb458eba7bae4a39c29d37405f685ffd172f694
cduranalarcon <44102364+cduranalarcon@users.noreply.github.com>
2019-12-09 15:19:47 +0100

    Update MRR_functions.py

--- Lib/MRR_functions.py
+++ Lib/MRR_functions.py
@@ -308,4 +308,37 @@ def save_Time_integ(file_out,tres = 1, mat = None, station=""):
         root_grp.close() 
     else:
         print "Data matrix not found!"
-            
+   
+
+def densplot(x,y,title="a)",bins = None, vmax = None, Range = None):
+    import numpy as np
+    import pylab
+    
+    def fmt(x, pos):
+        a, b = '{:.1e}'.format(x).split('e')
+        b = int(b)
+        #return r'${} \times 10^{{{}}}$'.format(a, b)
+        #print a == "0.0"
+        print a,b
+        if a != "0.0": 
+            return r'$'+str(a)+'\xc2\xb7'.decode('utf8')+' 10^{{'+str(b)+'}}$'
+        else:
+            return r'$'+str(0)+'$' 
+
+    h=np.histogram2d(x,y,bins=bins,range=Range)
+    histo_temp=np.ma.masked_where(h[0] <= 0, h[0])
+    extent = np.min(h[1]), np.max(h[1]), np.min(h[2]), np.max(h[2])
+
+    histo_temp=100*histo_temp/(1.*np.nansum(histo_temp))
+
+    if vmax==None: vmax=np.max(histo_temp)
+
+    im=pylab.imshow(np.transpose(np.flip(((histo_temp)),1)),interpolation="nearest",
+                    aspect='auto',extent=extent,vmin=0,vmax=vmax)
+    
+    #im=pylab.pcolor(np.linspace(0,2,bins[0]),np.linspace(0,2.9,bins[1]),
+    #                np.transpose(histo_temp),vmin=0,vmax=vmax)
+    
+    if title != '': pylab.title(title)
+    print extent
+    return im   
