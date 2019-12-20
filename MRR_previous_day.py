e8dd482bdcfcd833be3fafb352ae6a6e07879543
cduranalarcon <44102364+cduranalarcon@users.noreply.github.com>
2019-12-20 08:48:55 +0100

    Update MRR_previous_day.py

--- MRR_previous_day.py
+++ MRR_previous_day.py
@@ -3,7 +3,7 @@ import sys, os
 sys.path.append("lib/") # adding lib path
 from MRR_functions import check_RawFiles as crf  # Check Raw files for corrupted characters
 from MRR_functions import raw2snow # Process raw data into Doppler moments using MK2012
-from MRR_functions import QuickL #MRR QuickLooks
+from MRR_functions import QuickL_report #MRR QuickLooks for daily reports
 import time
 import matplotlib.pyplot as plt #Graphic package
 from MRR_functions import mkfolders # create output folders
@@ -51,7 +51,9 @@ year=yesterday[:4]
 month=yesterday[4:6] 
 day=yesterday[6:8]
 file_in = path+"RawSpectra/"+str(year)+str(month).zfill(2)+"/"+str(month).zfill(2)+str(day).zfill(2)+".raw"
-file_out = "Daily-schedules/"+name_station+"_"+str(year)+str(month).zfill(2)+str(day).zfill(2)+"_"+str(int(TRES))+"TRES.nc"
+file_out = "Daily-schedules/"+name_station+"_"+str(year)+str(month).zfill(2)+str(day).zfill(2)+"_"+str(int(TRES))+"TRES0.nc"
+file_out2 = "Daily-schedules/"+name_station+"_"+str(year)+str(month).zfill(2)+str(day).zfill(2)+"_"+str(int(TRES))+"TRES_report.nc"
+
 temp_file = path+"temp/rawfile.raw"
 
 if (os.path.isfile(file_in) == True): #Processes the data if file exists
@@ -60,6 +62,7 @@ if (os.path.isfile(file_in) == True): #Processes the data if file exists
 
     if n_errors > 0: 
         time.sleep(5) #Gives times to the temporal file, ti be created
+        print "Processing data of the day "+yesterday+" UTC"
         raw2snow(temp_file,file_out, TRES = TRES, Descr = Descr) # Convert Raw into Doppler Moments using MK2012
         time.sleep(5)  
 
@@ -74,6 +77,7 @@ if (os.path.isfile(file_in) == True): #Processes the data if file exists
 
     else:
         time.sleep(5) #Gives times to the temporal file, ti be created
+        print "Processing data of the day "+yesterday+" UTC"
         raw2snow(file_in,file_out, TRES = TRES, Descr = Descr)
 
         ds = Dataset(file_out,'a')
@@ -88,4 +92,10 @@ if (os.path.isfile(file_in) == True): #Processes the data if file exists
 else:
     print "Raw file not found "+str(year)+"/"+str(month).zfill(2)+"/"+str(day).zfill(2)
 
+ds = Dataset(file_out,'r')
+CopyDataset_simpler(file_out2,ds)
+ds.close()
 
+QuickL_report(year=yesterday[:4], month=yesterday[4:6], day=yesterday[6:8], name_station = 'DDU',TRES = TRES, Ze_ranges = [-20, 20],W_ranges = [-3, 3], SW_ranges = [0, 1], S_ranges = [0, 0.5], cmap = 'jet',format = 'png',dpi=200)        
+
+os.remove(file_out)
